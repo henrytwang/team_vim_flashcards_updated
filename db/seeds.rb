92f2c73257f97d6dbb4e28ff1a3b442361298dd6
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-17 11:09:02 -0500

    Add Decks to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,18 @@ require 'csv'
 
 
 headers = [:question, :answer]
+@aa = Deck.create(:name => "Amino Acids")
+@wc = Deck.create(:name => "World Capitals")
 
 
 CSV.foreach(APP_ROOT.join('public', 'aminos.csv')) do |row|
-  Card.create(Hash[headers.zip(row)])
+  hash = Hash[headers.zip(row)]
+  hash[:deck_id] = 1
+  Card.create(hash)
 end
 
 CSV.foreach(APP_ROOT.join('public', 'world_capitals.csv')) do |row|
-  Card.create(Hash[headers.zip(row)])
+  hash = Hash[headers.zip(row)]
+  hash[:deck_id] = 2
+  Card.create(hash)
 end
